4541becb8301a81be01b96a565a5d1976dadda3c
bhuban <bhubanghimire3130@gmail.com>
2022-09-22 12:33:49 +0545

    tringle

--- josephus.py
+++ josephus.py
@@ -1,17 +1,25 @@
+from re import I
 from unittest import result
 
 
 def josephus(n, k):
+    initial_list = [x+1 for x in range(n)]
     final_list = []
-    for i in range(n):
-        final_list.append(i+1)
+    index = 1
+    while (len(initial_list)>=k):
+        print(initial_list)
+        for index,item in enumerate(initial_list):
+            print(index, item)
+            if (index+1)%k==0:
+                final_list.append(item)
+                initial_list.remove(item)
 
+        print("ok")
+    final_list.extend(initial_list)
     return final_list
 
 
 n=4
 k=1
-
 result = josephus(n,k)
-
 print(result)
---
+++ right_triangle.py
@@ -0,0 +1,13 @@
+n=5
+
+for i in range(n):
+    for j in range(n-i):
+        print(j+1, end=" ")
+    print()
+
+print("\n")
+for i in range(n):
+    for j in range(i+1):
+        print(j+1, end=" ")
+    print()
+    
