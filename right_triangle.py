97e64e0a85225a9f1b09dde69a45a8b30f88b3f9
bhuban <bhubanghimire3130@gmail.com>
2022-09-28 14:54:53 +0545

    ok

--- right_triangle.py
+++ right_triangle.py
@@ -11,3 +11,12 @@ for i in range(n):
         print(j+1, end=" ")
     print()
     
+
+print("\n")
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j>i:
+            # print("",end=" ")
+        # else:
+            print("*", end=" ")
+    print()
